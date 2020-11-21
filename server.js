84f8ed90483e6d7459599e9ae55fc0cf85043feb
Numan Javed <nomanmani62@gmail.com>
2020-11-21 17:13:50 +0500

    add first GET route

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
 
+const userData = [
+  { name: 'Numan', id: 1 },
+  { name: 'Mani', id: 2 },
+];
+
+app.get('/users', (req, res) => {
+  res.json({
+    success: true,
+    message: 'Succesfully Got Users.',
+    users: userData,
+  });
+});
+
 app.listen(8000, () => {
   console.log('Server is Running on localhost 8000');
 });
